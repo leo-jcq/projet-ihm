e0f9beafe38a7a590f2d884676ef390f5815cc5c
Leo Jacquet <leojacquet2003@gmail.com>
2025-11-20 11:32:42 +0100

    setup eslint

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import pluginVue from 'eslint-plugin-vue';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import vueParser from 'vue-eslint-parser';
+
+export default defineConfig([
+    ...pluginVue.configs['flat/recommended'],
+    {
+        rules: {
+            'vue/max-attributes-per-line': 'off',
+            'vue/html-indent': ['error', 4],
+            'vue/html-self-closing': [
+                'error',
+                {
+                    html: {
+                        void: 'always'
+                    }
+                }
+            ],
+            'vue/singleline-html-element-content-newline': 'off'
+        },
+        languageOptions: {
+            sourceType: 'module',
+            globals: globals.browser,
+            parser: vueParser,
+            parserOptions: {
+                parser: tsParser,
+                ecmaVersion: 2020,
+                sourceType: 'module',
+                extraFileExtensions: ['.vue']
+            }
+        }
+    }
+]);
