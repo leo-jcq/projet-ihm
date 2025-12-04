7c0bbc2be94fd9e5be523818f0aa1349088653eb
Leo Jacquet <leojacquet2003@gmail.com>
2025-12-04 12:54:31 +0100

    configure path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: '/projet-ihm/'
+    base: '/projet-ihm/',
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src')
+        }
+    }
 });
